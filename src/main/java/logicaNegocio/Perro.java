bc9a55f5f097fa9e2b855674c2f9eb4b149741fe
julian <jcristancho838@gmail.com>
2024-11-18 10:23:06 -0500

    se añade la interfaz domestico

--- src/main/java/logicaNegocio/Perro.java
+++ src/main/java/logicaNegocio/Perro.java
@@ -1,5 +1,34 @@
 package logicaNegocio;
 
-public class Perro {
-    // TODO: Aquí va tu código
+public class Perro extends Animal implements Domestico{
+    
+    protected TipoHabitat habitat;
+
+    public Perro() {
+         this.habitat = TipoHabitat.TERRESTRE;
+    }
+
+
+    @Override
+    public String emitirSonido() {
+        return "Ladrido";
+    }
+
+
+    @Override
+    public String obtenerDieta() {
+        return "Omnivoro";
+    }
+
+    @Override
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
+    
+    @Override
+    public String interactuarConHumano(){
+        return "El perro mueve la cola y ladra de felicidad";
+    }
+    
+
 }
