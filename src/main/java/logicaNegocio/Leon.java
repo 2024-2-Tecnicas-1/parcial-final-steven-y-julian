1109f055e87ff07de9bc97702445eeb26f6b199d
julian <jcristancho838@gmail.com>
2024-11-18 09:59:43 -0500

    se creo la clase leon

--- src/main/java/logicaNegocio/Leon.java
+++ src/main/java/logicaNegocio/Leon.java
@@ -1,5 +1,27 @@
 package logicaNegocio;
 
-public class Leon {
-    // TODO: Aquí va tu código
+public class Leon extends Animal {
+
+    protected TipoHabitat habitat;
+
+    public Leon() {
+      this.habitat = TipoHabitat.TERRESTRE;  
+    }
+
+    @Override
+    public String emitirSonido() {
+        return "Rugido";
+    }
+
+    public String obtenerDieta() {
+        return "Carnívoro";
+    }
+
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
+
+  
+
 }
+
